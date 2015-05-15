10f899969338647cb534b6e928003c292404379d
Satoshi Sahara <sahara.satoshi@gmail.com>
2015-05-16 01:19:33 +0900

    Japanese lang files

---
+++ lang/ja/lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Japanese language file for anewssystem
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Satoshi Sahara <sahara.satoshi@gmail.com>
+ */
+/******************************************************************************/
+// anewssystem controls
+$lang['anss_input_btn_save']       = 'ニュース発行';
+$lang['anss_input_btn_save_descr'] = 'ニュース記事を発行します';
+
+/******************************************************************************/
+// anewssystem text
+$lang['news_added']    = 'Your article was stored successfully.';
+$lang['no_permission'] = 'Write access denied. You are not allowed to write news.';
+
+/******************************************************************************/
+// template related text
+$lang['anchordescr']     = 'unique id to reference the article';
+$lang['headdescr']       = 'give it an expressive headline';
+$lang['linkdescr']       = 'link the page where the news relate to';
+$lang['startdescr']      = 'first appearance of the article in the list';
+$lang['stopdescr']       = 'on this day the article will disappear';
+$lang['msgdescr']        = 'write a news summary';
+$lang['authordescr']     = 'author info, will be visible beside date info';
+$lang['tagdescr']        = 'as option for the syntax line, separate by pipe or blank if multiple';
+$lang['del_title']       = 'このニュース記事を削除する';
+$lang['newsflash_title'] = 'NEWS flash';
+$lang['anss_edit_imgttl']= 'このニュース記事を編集する';
+$lang['lnk_back']        = '&laquo; back';
+$lang['allnews']         = 'all News';
+$lang['noNews']          = '現在、お知らせはありません。';
