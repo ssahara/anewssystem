714ad2506fed295995d836a43457d0914f8810b3
Satoshi Sahara <sahara.satoshi@gmail.com>
2015-05-16 01:20:12 +0900

    Japanese setting file

---
+++ lang/ja/settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Japanese language file for anewssystem plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Taggic@t-online.de
+ * @author     Satoshi Sahara <sahara.satoshi@gmail.com>
+ */
+ 
+// for the configuration manager
+$lang['d_format']       = '日付表示形式 (例. Y-m-d)';
+$lang['news_datafile']  = 'Set the path to the namespace:newsdata file';
+$lang['prev_length']    = 'Define the quantity of previewed words per news post in NewsFlash';
+$lang['newsflash_link'] = 'provide News flash link';
+$lang['hide_anchorID']  = 'Anchor ID を表示しない';
+$lang['soapp']          = 'show only one article per page'; // soapp = show one article per page (instead of all news)
+$lang['yh_level']       = '全記事表示での年タイトルの見出しレベル';
+$lang['mh_level']       = '全記事表示での月タイトルの見出しレベル';
+$lang['h_level']        = '全記事表示での個別ニュース記事の見出しレベル';
+$lang['lnk_newsarchive']= 'アーカイブリンクのタイトル';
+$meta['act_delim']      = 'some templates / dw-version use questionmark instead of ampersand at action links';
+$lang['convert']        = 'Link a thumbshots online service';
